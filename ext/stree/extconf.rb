4b30058a2d7a1c195c3a14ae41ebf1723d2ee1b5
alanfoster <alan@alanfoster.me>
2020-04-26 18:31:50 +0100

    Add extconf configuration for pulling requiring libstree

--- ext/stree/extconf.rb
+++ ext/stree/extconf.rb
@@ -1,3 +1,44 @@
 require 'mkmf'
 
+LIBDIR = RbConfig::CONFIG['libdir']
+INCLUDEDIR = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/include',
+
+    # Then search /usr/local for people that installed from source
+    '/usr/local/include',
+
+    # Check the ruby install locations
+    INCLUDEDIR,
+
+    # Finally fall back to /usr
+    '/usr/include'
+]
+
+LIB_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/lib',
+
+    # Then search /usr/local for people that installed from source
+    '/usr/local/lib',
+
+    # Check the ruby install locations
+    LIBDIR,
+
+    # Finally fall back to /usr
+    '/usr/lib'
+]
+
+dir_config('stree', HEADER_DIRS, LIB_DIRS)
+
+unless find_header('stree/lst_string.h')
+  abort 'libstree is mising. please install libstree'
+end
+
+unless find_library('stree', 'lst_stree_free')
+  abort 'libstree is missing. please install libstree'
+end
+
 create_makefile('stree/stree')
